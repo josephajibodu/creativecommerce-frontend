5f3b08d0b94dfe0ca750769c400c12cdc47b51f7
cremirdevio <josephajibodu@gmail.com>
2024-05-30 22:33:05 +0100

    nav updatd

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -4,11 +4,7 @@ import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Company", href: "#" },
-];
+const navigation: { name: string; href: string }[] = [];
 
 const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
