beff52efe78fac10a16f05e48c708f57c8bfed25
cremirdevio <josephajibodu@gmail.com>
2024-06-25 03:22:10 +0100

    useAuth hook created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function Home() {
       {/* Join the waitlist */}
       <div className="">
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-white border  px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="relative isolate overflow-hidden bg-white border px-6 pt-16 pb-16 sm:pb-0 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
---
+++ src/hooks/auth.ts
@@ -0,0 +1,159 @@
+import useSWR from 'swr'
+import axios from '@/lib/axios'
+import { Dispatch, SetStateAction, useEffect } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+
+interface AuthHookProps {
+    middleware?: 'guest' | 'auth',
+    redirectIfAuthenticated?: string
+}
+
+interface RegisterProps {
+    name: string,
+    email: string,
+    password: string,
+    password_confirmation: string,
+}
+
+interface LoginProps {
+    email: string,
+    password: string,
+    remember?: boolean,
+}
+
+interface ResetPasswordProps {
+    email: string,
+    password: string,
+    password_confirmation: string,
+}
+
+interface ForgotPasswordProps {
+    email: string
+}
+
+type ErrorProps = {
+    setErrors: Dispatch<SetStateAction<[]>>
+}
+
+type StatusProps = {
+    setStatus: Dispatch<SetStateAction<string | null>>
+}
+
+export const useAuth = ({ middleware, redirectIfAuthenticated = '/home' }: AuthHookProps = {}) => {
+    const router = useRouter()
+    const params = useParams()
+
+    const { data: user, error, mutate } = useSWR('/api/user', () =>
+        axios
+            .get('/api/user')
+            .then(res => res.data)
+            .catch(error => {
+                if (error.response.status !== 409) throw error
+
+                router.push('/verify-email')
+            }),
+    )
+
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+    const register = async ({ setErrors, ...props }: RegisterProps & ErrorProps) => {
+        await csrf()
+
+        setErrors([])
+
+        axios
+            .post('/register', props)
+            .then(() => mutate())
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const login = async ({ setErrors, setStatus, ...props }: LoginProps & ErrorProps & StatusProps) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/login', props)
+            .then(() => mutate())
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const forgotPassword = async ({ setErrors, setStatus, email }: ForgotPasswordProps & ErrorProps & StatusProps) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/forgot-password', { email })
+            .then(response => setStatus(response.data.status))
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const resetPassword = async ({ setErrors, setStatus, ...props }: ResetPasswordProps & ErrorProps & StatusProps) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/reset-password', { token: params.token, ...props })
+            .then(response =>
+                router.push('/login?reset=' + btoa(response.data.status)),
+            )
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const resendEmailVerification = ({ setStatus }: StatusProps) => {
+        axios
+            .post('/email/verification-notification')
+            .then(response => setStatus(response.data.status))
+    }
+
+    const logout = async () => {
+        if (!error) {
+            await axios.post('/logout').then(() => mutate())
+        }
+
+        window.location.pathname = '/login'
+    }
+
+    useEffect(() => {
+        if (middleware === 'guest' && redirectIfAuthenticated && user)
+            router.push(redirectIfAuthenticated)
+        if (
+            window.location.pathname === '/verify-email' &&
+            user?.email_verified_at
+        )
+            router.push(redirectIfAuthenticated)
+        if (middleware === 'auth' && error) logout()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user, error])
+
+    return {
+        user,
+        register,
+        login,
+        forgotPassword,
+        resetPassword,
+        resendEmailVerification,
+        logout,
+    }
+}
