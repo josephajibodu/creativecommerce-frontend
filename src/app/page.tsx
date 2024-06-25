489f069b854d7ffc0ce3a0932428ce0fa3d6d63a
cremirdevio <josephajibodu@gmail.com>
2024-06-25 03:34:30 +0100

    landing page fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,15 +106,14 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="text-base font-semibold leading-7 text-orange-700">
-              Deploy faster
+              Stay in Charge
             </h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Everything you need to deploy your app
+              Empower Your E-commerce Journey
             </p>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-              Suspendisse eget egestas a elementum pulvinar et feugiat blandit
-              at. In mi viverra elit nunc.
+              Choose from customizable frontends, affordable solutions, and
+              flexible hosting options tailored to your business needs.
             </p>
           </div>
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-7xl">
